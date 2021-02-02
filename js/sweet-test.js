f67a10c17aeb7d5e2c8c25ae8235120933e13937
何甜甜 <1731098360@qq.com>
2021-02-02 14:57:04 +0800

    测试

---
+++ js/sweet-test.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         sweet-脚本测试
+// @namespace    https://greasyfork.org/
+// @version      1.0
+// @description  sweet-脚本测试，自己写着玩的
+// @author       sweet
+// @include      *baidu.com*
+// @include      *google*
+// @require https://cdn.staticfile.org/vue/2.6.11/vue.min.js
+// @require https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    alert("sweet-Hello World");
+    console.log("sweet");
+})();
