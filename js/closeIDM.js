29ca5f35e575b11a70481f4932a1e592d251f661
何甜甜 <1731098360@qq.com>
2021-02-08 21:31:24 +0800

    修改脚本名称

--- js/closeIDM.js
+++ js/closeIDM.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         关闭IDM提示去官网下载的弹窗
+// @name         关闭IDM提示去官网下载的标签页
 // @namespace    https://greasyfork.org/zh-CN/scripts/421429
 // @version      1.0
 // @description  使用IDM破解版后经常提示不安全并自动弹出去官网下载的页面，很烦人！！！故写了个脚本关闭该页面
