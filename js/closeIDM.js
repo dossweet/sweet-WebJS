ab9ae4025c942f06dbdcb765391ea5c99adc377a
何甜甜 <1731098360@qq.com>
2021-02-08 18:04:00 +0800

    新增关闭IDM弹窗的插件

--- js/closeIDM.js
+++ js/closeIDM.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         关闭IDM提示去官网下载的弹窗
-// @namespace    https://greasyfork.org/
+// @namespace    https://greasyfork.org/zh-CN/scripts/421429
 // @version      1.0
 // @description  使用IDM破解版后经常提示不安全并自动弹出去官网下载的页面，很烦人！！！故写了个脚本关闭该页面
 // @author       sweet
