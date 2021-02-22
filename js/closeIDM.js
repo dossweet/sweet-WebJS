dac6f2271eb42f5a5dbc9804da190de0e89de934
何甜甜 <1731098360@qq.com>
2021-02-22 22:37:55 +0800

    加快拦截速度

--- js/closeIDM.js
+++ js/closeIDM.js
@@ -1,12 +1,15 @@
 // ==UserScript==
 // @name         关闭IDM提示去官网下载的标签页
 // @namespace    https://greasyfork.org/zh-CN/scripts/421429
-// @version      1.0
+// @version      1.1
 // @description  使用IDM破解版后经常提示不安全并自动弹出去官网下载的页面，很烦人！！！故写了个脚本关闭该页面
 // @author       sweet
 // @match       https://www.internetdownloadmanager.com/download.html?lng=chn2
+// @run-at      document-start
 // @icon        https://cdn.jsdelivr.net/gh/doublesweet01/BS_script@master/image/sweet.jpg
 // @license     MIT
+// @note        v1.0实现拦截功能
+// @note        v1.1加快拦截速度
 // ==/UserScript==
 (function() {
     'use strict';
