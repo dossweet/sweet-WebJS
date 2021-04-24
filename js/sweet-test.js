f7d0e30008c9f8b0df0dc8208fe0dbcab28f4d20
何甜甜 <1731098360@qq.com>
2021-04-24 16:10:06 +0800

    修改greasyfork地址

--- js/sweet-test.js
+++ js/sweet-test.js
@@ -6,15 +6,34 @@
 // @author       sweet
 // @include      *baidu.com*
 // @include      *google*
+// @icon https://cdn.jsdelivr.net/gh/doublesweet01/BS_script@master/image/sweet.jpg
 // @require https://cdn.staticfile.org/vue/2.6.11/vue.min.js
 // @require https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.min.js
-// @grant        none
+// @grant    GM_getValue
+// @grant    GM.getValue
+// @grant    GM_setValue
+// @grant    GM.setValue
+// @grant    GM_addStyle
+// @grant    GM_getResourceURL
+// @grant    GM_listValues
+// @grant    GM_getResourceUrl
+// @grant    GM.getResourceUrl
+// @grant    GM_xmlhttpRequest
+// @grant    GM_getResourceText
+// @grant    GM_registerMenuCommand
+// @grant    unsafeWindow
+// @license  MIT
 // ==/UserScript==
 
 (function() {
     'use strict';
+    console.log("sweet-script start...");
+    // 隐藏的class集合
+    var classArray = ['before-content','lemmaWgt-promotion-vbaike','topA','appdownload'];
+    // 隐藏操作
+    function hideClass(className){
+        var hideEle = document.getElementsByClassName('.'+className);
+        // hideEle.style.
+    }
 
-    // Your code here...
-    alert("sweet-Hello World");
-    console.log("sweet");
 })();
