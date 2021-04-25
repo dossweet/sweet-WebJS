70b7b1183b4a6adc1220dcf915f0d91ec3cd6226
何甜甜 <1731098360@qq.com>
2021-04-25 14:29:12 +0800

    实现免登录复制

--- js/csdnOptimize.js
+++ js/csdnOptimize.js
@@ -1,13 +1,14 @@
 // ==UserScript==
-// @name         CSDN文章阅读优化，过滤推荐文章模块中的下载专区&免登录查看所有评论&去广告
+// @name         CSDN文章阅读优化，过滤相关推荐中的下载模块&免登录查看所有评论&去广告&免登录复制
 // @namespace    https://greasyfork.org/zh-CN/scripts/425479
-// @version      3.0
+// @version      4.0
 // @description  csdn文章阅读界面下侧的相关文章中会有下载模块，但是一般我们只是想看文章，不想跳到相关下载，因此想写个脚本过滤掉下载模块，同时还实现免登录查看所有评论的功能，评论保留翻页功能,页面实现去广告
 // @author       sweet
 // @include      *://blog.csdn.net/*/article/details/*
 // @run-at      document-idle
 // @icon        https://cdn.jsdelivr.net/gh/doublesweet01/BS_script@master/image/sweet.jpg
 // @license     GPL-3.0-only
+// @note        v4.0实现免登录复制
 // @note        v3.0实现去广告
 // @note        v2.1完善免登录展开评论功能，评论可以自动翻页，现在不会弹登录窗口了
 // @note        v2.0实现免登录展开评论功能，评论可以自动翻页
@@ -26,13 +27,16 @@
 // @grant    GM_xmlhttpRequest
 // @grant    GM_getResourceText
 // @grant    GM_registerMenuCommand
+// @grant    GM_setClipboard
 // @grant    unsafeWindow
 // ==/UserScript==
 (function () {
     'use strict';
     // 待隐藏的class名称集合
-    var classNameArray = ['type_download', 'comment-list-box', 'text-center', 'd-none','login-box','login-mark'];
+    var classNameArray = ['type_download', 'comment-list-box', 'text-center', 'd-none', 'login-mark'];
     var idNameArray = ['recommendAdBox'];
+    var addStyle = GM_addStyle;
+    var setClipboard = GM_setClipboard;
 
     // 隐藏元素
     function hideEle(className) {
@@ -47,34 +51,18 @@
                 break;
             case 'comment-list-box':
                 let hideElement01 = document.querySelector('.' + className);
-                if (hideElement01){
+                if (hideElement01) {
                     hideElement01.style = "";
                 }
                 break;
             case 'text-center':
-                let hideElement02 = document.querySelector('.' + className);
-                if (hideElement02){
-                    hideElement02.style.display = "none";
-                }
+                addStyle(".text-center{display:none;}");
                 break;
             case 'd-none':
-                let hideElement03 = document.querySelector('.' + className);
-                if (hideElement03){
-                    hideElement03.style.removeProperty("display");
-                    hideElement03.style.setProperty("display","block","important");
-                }
-                break;
-            case 'login-box':
-                let hideElement04 = document.querySelector('.' + className);
-                if (hideElement04){
-                    hideElement04.style.display = "none";
-                }
+                addStyle(".d-none{display:block !important;}");
                 break;
             case 'login-mark':
-                let hideElement05 = document.querySelector('.' + className);
-                if (hideElement05){
-                    hideElement05.style = "";
-                }
+                addStyle(".login-mark,#passportbox{display:none !important;}");
                 break;
         }
     }
@@ -92,7 +80,6 @@
                 break;
         }
     }
-
 
     //对于不同类的元素，写个方法来一起修正css样式
     function removeElements(classNameArray) {
@@ -116,12 +103,24 @@
         }, times);
     }
 
+    function copyNoLogin(){
+        $(".hljs-button").attr("data-title", "免登录复制");
+        $(".hljs-button").click(function(){
+            setClipboard(this.parentNode.innerText);//获取复制内容
+            $(".hljs-button").attr("data-title", "复制成功");
+            setTimeout(function(){
+                $(".hljs-button").attr("data-title", "免登录复制");
+            }, 1000);
+        });
+    }
+
     //打印脚本信息
     function printScript() {
-        console.log("sweet-CSDN文章阅读优化，过滤相关推荐模块的下载部分以及免登录查看全部评论~")
+        console.log("sweet-CSDN文章阅读优化，过滤相关推荐模块的下载部分&免登录查看全部评论&免登录复制&去广告~")
     }
 
     safeWaitFunc(classNameArray, removeElements);
     safeWaitFunc(idNameArray, removeElementsById);
+    copyNoLogin();
     printScript();
 })();
