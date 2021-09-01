c0bd879385a7f1413b1e8cce3646e539f34cd31d
何甜甜 <1731098360@qq.com>
2021-09-01 10:20:31 +0800

    提升版本号

--- js/csdnOptimize.js
+++ js/csdnOptimize.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSDN文章阅读优化，过滤相关推荐中的下载模块&免登录查看所有评论&去广告
 // @namespace    https://greasyfork.org/zh-CN/scripts/425479
-// @version      5.1
+// @version      6.0
 // @description  csdn文章阅读界面下侧的相关文章中会有下载模块，但是一般我们只是想看文章，不想跳到相关下载，因此想写个脚本过滤掉下载模块，同时还实现免登录查看所有评论的功能，评论保留翻页功能,页面实现去广告
 // @author       sweet
 // @include      *://blog.csdn.net/*/article/details/*
